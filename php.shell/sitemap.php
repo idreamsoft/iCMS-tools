b4e3d44c851f0992e081e6dbd08ce47f1025982f
cool.tea <idreamsoft@qq.com>
2015-12-05 22:48:57 +0800

    u

--- php.shell/sitemap.php
+++ php.shell/sitemap.php
@@ -4,24 +4,28 @@ require_once(dirname(__FILE__).'/../iCMS.php');
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('memory_limit','2048M');
 
-$path = iPATH.'sitemap' ;
 iPHP::$iTPL_MODE = "html";
-$xml = iPHP::view('iCMS://sitemap.index.static.htm');
-iFS::mkdir($path);
-iFS::write($path.'/index.xml',$xml);
+
+$sitemap_dir  = 'sitemap';
+$sitemap_path = iFS::path(iPATH.iCMS::$config['router']['html_dir'].$sitemap_dir);
+$sitemap_url  = iCMS_URL.'/'.$sitemap_dir;
+
+$xml       = iPHP::view('iCMS://sitemap.index.static.htm');
+iFS::mkdir($sitemap_path);
+iFS::write($sitemap_path.'/index.xml',$xml);
 
 $tag = iPHP::view('iCMS://sitemap.baidu.tag.htm');
-iFS::mkdir($path);
-iFS::write($path.'/tag.xml',$tag);
-
+iFS::mkdir($sitemap_path);
+iFS::write($sitemap_path.'/tag.xml',$tag);
 
 $category   = iDB::all("SELECT `cid` FROM `#iCMS@__category` where `rootid`='0' and `status` ='1' and `appid` ='1' order by cid asc");
-
 
 foreach ((array)$category as $key => $cat) {
 	iPHP::assign('cid',(int)$cat['cid']);
 	$xml = iPHP::view('iCMS://sitemap.baidu.htm');
-
-	iFS::write($path.'/'.$cat['cid'].'.xml',$xml);
-	echo $path.'/'.$cat['cid'].".xml\n";
+    $xmlPath = $sitemap_path.'/'.$cat['cid'].'.xml';
+	iFS::write($xmlPath,$xml);
+	echo $xmlPath.PHP_EOL;
 }
+echo $sitemap_url.'/index.xml'.PHP_EOL;
+echo $sitemap_url.'/tag.xml'.PHP_EOL;
